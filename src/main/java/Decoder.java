793f7da109674c47eb7fd04c90a0604d4fde470e
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-04-15 09:07:11 -0400

    starting logic

--- src/main/java/Decoder.java
+++ src/main/java/Decoder.java
@@ -1,7 +1,11 @@
 public class Decoder {
 
-    public static String decode (String encoded) {
+    public static String decode(String encoded) {
+        String[] str1 = {"XOXXXO", "XXXOXOXOXO", "XXXOXOXXXOXO", "XXXOXOXOXXXO", "XO", "XOXOXXXOXO", "XXXOXXXOXO", "XOXOXOXO", "XOXO", "XOXXXOXXXOXXXO", "XXXOXOXXXO", "XOXXXOXOXO", "XXXOXXX", "XXXOXO", "XXXOXXXOXXXO", "XOXXXOXXXOXO", "XXXOXXXOXOXXXO", "XOXXXOXO", "XOXOXO", "XXXO", "XOXOXXXO", "XOXXXOXXXO", "O", "XXXOXOXXXOXXXO", "XXXOXXXOXOXO", "XXXOXXXOXOXO "};
+        String[] str2 = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
         return null;
     }
 }
+
+
